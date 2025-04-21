e1c713dbafecd96b06d3707fe008d1699c993319
pawel-laptop <p.blaszczy@gmail.com>
2025-04-21 15:27:27 +0200

    create /predict endpoint

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
 
+from models.api import PredictRequest, PredictResponse
+
 app = FastAPI(version="0.0.1")
+
+
+@app.post("/predict")
+async def predict(data: PredictRequest) -> PredictResponse:
+    """
+    Dummy prediction endpoint.
+    """
+    # Simulate a prediction process
+    return PredictResponse(prediction="neutral")
--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -1,5 +1,26 @@
+from fastapi.testclient import TestClient
+
 from app import app
+from models.api import PredictRequest, PredictResponse
+
+
+client = TestClient(app)
 
 
 def test_can_import_app():
     assert app is not None
+
+
+def test_predict_accepts_json():
+    req = PredictRequest(text="test")
+    response = client.post("/predict", json=req.model_dump())
+
+    assert response.status_code == 200
+
+
+def test_predict_response():
+    req = PredictRequest(text="test")
+    response = client.post("/predict", json=req.model_dump())
+
+    assert response.status_code == 200
+    assert response.json() == PredictResponse(prediction="neutral").model_dump()
